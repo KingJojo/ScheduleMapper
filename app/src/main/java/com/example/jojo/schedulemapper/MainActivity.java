45e961593aee2bdff96e6018b9f61ceda1722788
KingJojo <funnygoman@gmail.com>
2015-10-25 13:48:12 -0700

    Revoking Previous Commit

--- app/src/main/java/com/example/jojo/schedulemapper/MainActivity.java
+++ app/src/main/java/com/example/jojo/schedulemapper/MainActivity.java
@@ -17,9 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        System.out.println(hi);
-
+        
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
